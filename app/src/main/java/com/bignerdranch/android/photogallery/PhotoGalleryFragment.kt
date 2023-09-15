2c227de23464e9eedd97a988d34b2f655012ef2f
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-15 15:44:40 -0700

    Ch20: Adds GalleryItem model class. Adds ksp plugin and then adds moshi converter.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -27,7 +27,7 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPhotoGalleryBinding.inflate(inflater, container, false)
         binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
         return binding.root
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/GalleryItem.kt
@@ -0,0 +1,3 @@
+package com.bignerdranch.android.photogallery.api
+
+data class GalleryItem(val title: String, val id: String, val url: String)
