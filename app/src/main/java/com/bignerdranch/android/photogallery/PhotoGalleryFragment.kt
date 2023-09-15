180e9a0e66d61e726a3627de9e680e78f6f6e61a
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-15 14:16:38 -0700

    Ch20: Moves toward repository pattern by adding PhotoRepository that wraps networking code.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -8,11 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -39,14 +36,8 @@ class PhotoGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-        val flickrApi = retrofit.create(FlickrApi::class.java)
-
         viewLifecycleOwner.lifecycleScope.launch {
-            val response = flickrApi.fetchContents()
+            val response = PhotoRepository().fetchContents()
             Log.d(TAG, "Response received: $response")
         }
     }
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.photogallery
+
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class PhotoRepository {
+
+    private var flickrApi: FlickrApi
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    suspend fun fetchContents() = flickrApi.fetchContents()
+}
