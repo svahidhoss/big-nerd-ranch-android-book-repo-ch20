2c8aa705d259c223493fb53c59ad2d8b85d04e2b
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-15 14:02:49 -0700

    Ch20: Adds Internet permission. Adds Scalar converters to deserialize the http responses. Executes the web GET request and logs it.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,14 +1,20 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 /**
  * A simple [Fragment] subclass.
@@ -33,8 +39,16 @@ class PhotoGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/").build()
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
         val flickrApi = retrofit.create(FlickrApi::class.java)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response = flickrApi.fetchContents()
+            Log.d(TAG, "Response received: $response")
+        }
     }
 
     override fun onDestroyView() {
