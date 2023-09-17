0cb9f61acbad7f4c8b7543eede5c4d731a9e6c7c
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-17 13:36:47 -0700

    Ch20: Updates the root build file to use the latest kotlin plugin version compatible with ksp plugin. Updates build file to contain the moshi kotlin dependency. Adds Kotlin moshi reflection adapter so that retrofit can correctly maps json data to Kotlin objects.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.photogallery
 
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -11,9 +13,14 @@ class PhotoRepository {
     private var flickrApi: FlickrApi
 
     init {
+        // Kotlin reflection adapter
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
