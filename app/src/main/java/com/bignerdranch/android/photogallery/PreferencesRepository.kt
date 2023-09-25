604d5e76bd0412cef4bfe332ca253e0f74bf9dfa
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-25 09:51:10 -0700

    Ch21: Makes PreferencesRepository singleton and initiates it in PhotoGalleryApplication class and registers it in manifest file.

--- app/src/main/java/com/bignerdranch/android/photogallery/PreferencesRepository.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PreferencesRepository.kt
@@ -1,12 +1,16 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import java.lang.IllegalStateException
 
 class PreferencesRepository(private val dataStore: DataStore<Preferences>) {
     val storedQuery: Flow<String> = dataStore.data.map {
@@ -21,5 +25,21 @@ class PreferencesRepository(private val dataStore: DataStore<Preferences>) {
 
     companion object {
         private val SEARCH_QUERY_KEY = stringPreferencesKey("search_query")
+        private var INSTANCE: PreferencesRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                val dataStore = PreferenceDataStoreFactory.create {
+                    context.preferencesDataStoreFile("settings")
+                }
+                INSTANCE = PreferencesRepository(dataStore)
+            }
+        }
+
+        fun get(): PreferencesRepository {
+            return INSTANCE ?: throw IllegalStateException(
+                "PreferencesRepository must be initialized"
+            )
+        }
     }
 }
